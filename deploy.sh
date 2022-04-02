b92b9fb3d6e984c002e54740155cf216dae97356
gosseynx <pascatgm@gmail.com>
2022-04-02 13:04:59 +0200

    Mise à jour du blog le 02/:m/2022 à 13:04

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-set -e
-set -u
-set -x
+#set -e
+#set -u
+#set -x
 
 
 #########  script de déploiement automatique lancé par easy-jekyll ##########
 
-DATE=$(date +%d%m%Y-%H%M)
+DATE1=$(date +%d/:m/%Y)
+DATE2=" à "
+DATE3=$(date +%H:%M) 
+DATE4=$DATE1$DATE2$DATE3
 
 git co master
 # élimination des fichiers temporaires
 ~/bin/nettoie.sh
 git add -A
-git ci -m "Mise à jour du blog le ${DATE}"
+git ci -m "Mise à jour du blog le ${DATE4}"
 git push
