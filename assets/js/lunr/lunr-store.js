658a988f5000fd5b16b1aba300d6a950eb3e4117
gosseynx <gosseynx@users.noreply.github.com>
2024-01-22 04:09:48 +0000

    jekyll build from Action 735c36e17b95a501327ed0bf1f7a8f41f9ab9ade

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "l'hypothèse de Riemann",
+        "excerpt":"L’hypothèse de Riemann Les problèmes du Prix du Millénaire L’institut pour les Mathématiques Clay (CMI), un institut scientifique privé britannique situé à Oxford a publié le 24 Mai 2004 une liste de sept problèmes mathématiques (assorti d’un prix d’un million de dollars pour la résolution de chacun d’eux) considérés comme...","categories": ["Mathématiques"],
+        "tags": ["Mathématiques"],
+        "url": "/math%C3%A9matiques/HypotheseDeRiemann/",
+        "teaser": null
+      },{
+        "title": "Emacs easy-jekyll",
+        "excerpt":"Blogger avec easy-jekyll depuis Emacs Publier des articles avec emacs pour Jekyll peut être à la fois simple et entièrement automatisé en utilisant le package easy-jekyll. À une seule condition : la configuration initiale de Jekyll recommande d’utiliser des gems Ruby sur une base locale et de les conserver sous...","categories": ["Linux"],
+        "tags": [],
+        "url": "/linux/Emacseasy-jekyll/",
+        "teaser": null
+      },{
+        "title": "Le problème de Bâle",
+        "excerpt":"Une brillante illustration du problème de Bâle Rappel historique Le problème de Bâle (ou Basel Problem) a longtemps résisté aux mathématiciens. Il consiste à déterminer la somme de la série : \\[\\frac{1}{1^2} + \\frac{1}{2^2} + \\frac{1}{3^2}+ \\dotsb \\frac{1}{n^2} + \\dotsb\\] Cette série converge lentement (pour obtenir \\(4\\) décimales exactes il...","categories": ["Mathématiques"],
+        "tags": [],
+        "url": "/math%C3%A9matiques/Basel/",
+        "teaser": null
+      },{
+        "title": "Prix Abel 2022",
+        "excerpt":"Prix Abel 2022 Les prix mathématiques En1 l’absence de prix Nobel dédié aux mathématiques, il existe non pas un mais deux prix pour distinguer les auteurs de contributions exceptionnelles à cette discipline : la Médaille Fields décernée tous les quatre ans à un mathématicien de moins de quarante ans pour...","categories": ["Mathématiques"],
+        "tags": [],
+        "url": "/math%C3%A9matiques/PrixAbel2022/",
+        "teaser": null
+      },{
+        "title": "Mathématiques-Intelligence Artificielle",
+        "excerpt":"IA et Mathématiques Lorsqu’il s’agit de rendre compte des avancées de l’intelligence artificielle, il convient d’être particulièrement attentif à ne pas sombrer dans les deux écueils symétriques mais aussi stériles l’un que l’autre que constituent l’émerveillement béat et le catastrophisme apocalyptique. Ce qui suit relate l’émergence d’une collaboration inédite entre...","categories": ["Mathématiques"],
+        "tags": ["Mathématiques","IA"],
+        "url": "/math%C3%A9matiques/Math%C3%A9matiques-IA/",
+        "teaser": null
+      },{
+        "title": "La conjecture de Collatz",
+        "excerpt":"De la dangerosité des conjectures mathématiques Le problème de Collatz Comme il est expliqué dans cette vidéo (en anglais), l’énoncé du problème de Collatz est très simple : A partir du choix d’un entier naturel \\(n\\) on applique la règle suivante : s’il est impair on le remplace par \\(3n+1\\)...","categories": [],
+        "tags": ["Mathématiques"],
+        "url": "/Collatz/",
+        "teaser": null
+      },{
+        "title": "staticman",
+        "excerpt":"Comments for static sites staticman I recently considered adding comments to my blog and after studying several alternatives (as disqus for instance) I decided to choose staticman due to its open source nature and also because it keeps all the comments in your repository on GitHub and doesn’t save them...","categories": ["Informatique"],
+        "tags": [],
+        "url": "/informatique/staticman/",
+        "teaser": null
+      }]
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#set -e
+#set -u
+#set -x
+
+
+#########  script de déploiement automatique lancé par easy-jekyll ##########
+
+DATE1=$(date +%d/:%m/%Y)
+DATE2=" à "
+DATE3=$(date +%H:%M) 
+DATE4=$DATE1$DATE2$DATE3
+
+git co master
+# élimination des fichiers temporaires
+~/bin/nettoie.sh
+git add -A
+git ci -m "Mise à jour du blog le ${DATE4}"
+git push
